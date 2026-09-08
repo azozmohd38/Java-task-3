218ba3c7c7894e42914d362878166100cdeb1452
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 09:08:38 +0400

    Task 329 - validate triangle base input

--- src/TriangleCalculator.java
+++ src/TriangleCalculator.java
@@ -19,6 +19,11 @@ public class TriangleCalculator {
 
         // Prompt the user to enter the base
         System.out.print("Enter the base of the triangle: ");
+        while (!input.hasNextDouble()) {
+            System.out.println("Please enter a valid number.");
+            input.next();
+            System.out.print("Enter the base of the triangle: ");
+        }
         base = input.nextDouble();
 
         // Prompt the user to enter the height
