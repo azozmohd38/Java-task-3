8096f8290dd7b2270ee31482fc988e1c8215c804
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 09:23:03 +0400

    Task 337 - validate deposit amount

--- src/BankAccountSystem.java
+++ src/BankAccountSystem.java
@@ -28,6 +28,11 @@ public class BankAccountSystem {
 
         boolean found = false;
 
+        if (amount <= 0) {
+            System.out.println("Deposit amount must be greater than zero.");
+            return;
+        }
+
         for (int i = 0; i < accountNumbers.size(); i++) {
             if (accountNumbers.get(i) == accountNumber) {
                 balances.set(i, balances.get(i) + amount);
