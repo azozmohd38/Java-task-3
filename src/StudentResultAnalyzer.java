f53abe95bf41074e933721e13eb08f515df84084
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 09:18:59 +0400

    Task 334 - format average marks

--- src/StudentResultAnalyzer.java
+++ src/StudentResultAnalyzer.java
@@ -54,7 +54,7 @@ public class StudentResultAnalyzer {
         System.out.println("English: " + english);
         System.out.println("Computer: " + computer);
         System.out.println("Total Marks: " + total);
-        System.out.println("Average Marks: " + average);
+        System.out.printf("Average Marks: %.2f%n", average);
         System.out.println("Final Grade: " + grade);
         System.out.println("Performance Status: " + performance);
     }
