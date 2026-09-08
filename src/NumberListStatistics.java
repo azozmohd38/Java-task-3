bd52f644cc7fa480e87f6d028dabc13dbde17ea5
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 09:14:13 +0400

    Task 332 - fix number list main method

--- src/NumberListStatistics.java
+++ src/NumberListStatistics.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NumberListStatistics {
-    public static void main() {
+    public static void main(String[] args) {
 
 
         // Create a List to store integer numbers
